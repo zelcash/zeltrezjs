2fe2096b433cfd912f473e56b317a4bf4552bfc6
TheTrunk <TheTrunk@users.noreply.github.com>
2018-04-25 12:08:28 +0200

    zeltrez

--- src/config.js
+++ src/config.js
@@ -1,30 +1,30 @@
 /*
-config.js - Configuration for Zelcash
+config.js - Configuration for Litecoin
 */
 
 module.exports = {
-    mainnet: {
-        messagePrefix: 'Zelcash mainnet',
-        bip32: {
-            public: '0488b21e',
-            private: '0488ade4'
-        },
-        pubKeyHash: '1cb8',
-        scriptHash: '1cbd',
-        zcPaymentAddressHash: '169a',
-        zcSpendingKeyHash: 'ab36',
-        wif: '80'
+  mainnet: {
+    messagePrefix: 'Litecoin mainnet',
+    bip32: {
+      public: '0488b21e',
+      private: '0488ade4'
     },
-    testnet: {
-        messagePrefix: 'Zelcash testnet',
-        bip32: {
-            public: '043587cf',
-            private: '04358394'
-        },
-        pubKeyHash: '1d25',
-        scriptHash: '1cba',
-        zcPaymentAddressHash: '16b6',
-        zcSpendingKeyHash: 'ac08',
-        wif: 'ef'
-    }
+    pubKeyHash: '30',
+    scriptHash: '05', //or 32
+    zcPaymentAddressHash: '169a', //not in ltc
+    zcSpendingKeyHash: 'ab36', //not in ltc
+    wif: 'b0'
+  },
+  testnet: {
+    messagePrefix: 'Litecoin testnet',
+    bip32: {
+      public: '043587cf',
+      private: '04358394'
+    },
+    pubKeyHash: '6f',
+    scriptHash: 'c4', //or 3a
+    zcPaymentAddressHash: '16b6', //not in ltc
+    zcSpendingKeyHash: 'ac08', //not in ltc
+    wif: 'ef'
+  }
 };
